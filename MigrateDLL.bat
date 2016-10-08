bdec87da0e513ce593a9e35e4f292e84aca4949c
Alexander Shoulson <ashoulson@gmail.com>
2016-10-08 19:39:20 -0400

    Reorganizing and restructuring

--- MigrateDLL.bat
+++ MigrateDLL.bat
@@ -3,8 +3,9 @@ REM Automatically migrates DLL files from the VS solution into Unity
 REM and generates MDBs. Unity's included PDB2MDB crashes on these DLLs
 REM so we use a newer one from Mono itself included in the Util directory.
 SET ROOT_PATH=%~dp0
-SET ASSEMBLY_PATH=%~dp0.\Unity\Assets\Assemblies\Generated\
+SET ASSEMBLY_PATH=%~dp0.\UnityClient\Assets\Assemblies\Generated\
 SET PROJECT_PATH=%1
+SET PDB2MDB_PATH=pdb2mdb.exe
 
 ECHO DLL MIGRATION BEGIN 
 ECHO Source: %PROJECT_PATH%
@@ -19,7 +20,7 @@ ECHO f | xcopy "%PROJECT_PATH%*.pdb" "%ASSEMBLY_PATH%" /Y
 FOR %%i IN ("%ASSEMBLY_PATH%*.dll") DO (
    IF EXIST "%%~npi.pdb" (
       ECHO Generating MDB for %%i...
-	  "%ROOT_PATH%.\Util\pdb2mdb.exe" "%%i"
+	  "%ROOT_PATH%.\%PDB2MDB_PATH%" "%%i"
    )
 )
 ECHO DLL MIGRATION FINISHED
